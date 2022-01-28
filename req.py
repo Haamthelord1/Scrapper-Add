58a4c975ae6b20687791b017387e7eab29f1a237
Haamthelord1 <94069564+Haamthelord1@users.noreply.github.com>
2022-01-28 12:06:47 +0530

    Add files via upload
    Use python req.py, to install all the requirements to execute this script.
    Use python ma.py, to manage everything at one place.

---
+++ req.py
@@ -0,0 +1,6 @@
+import os
+def main():
+ os.system('pip install colorama')
+ os.system('pip3 install telethon')
+ os.system('pip install requests')
+main()
---
+++ update.py
@@ -0,0 +1,7 @@
+import os
+ #We will update Every requirement
+def main():
+ os.system('pip3 install --upgrade colorama')
+ os.system('pip3 install --upgrade telethon')
+ os.system('pip3 install --upgrade requests')
+main()
